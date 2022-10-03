c739718d768174a09cf1cc5a0034bd0aff52f675
bertrand <mutanganabertrand@gmail.com>
2022-10-03 11:54:54 +0200

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
-  devise_for :users
   root "users#index"
+  devise_for :users
+  resources :users, only: [:index, :show]
 end
