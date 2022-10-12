46aa270fa1bbc1721bba0ed625bc1123598e4761
bertrand <mutanganabertrand@gmail.com>
2022-10-12 17:47:11 +0200

    Add default value to posts columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_121154) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_154108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,8 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_121154) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "comments_count"
-    t.integer "likes_count"
+    t.integer "comments_count", default: 0
+    t.integer "likes_count", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
