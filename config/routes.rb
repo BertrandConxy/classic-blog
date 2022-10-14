605d5374d945f2e0a0866376e28d5d998f951d6e
bertrand <mutanganabertrand@gmail.com>
2022-10-14 10:06:53 +0200

    Comment created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/destroy'
   root "users#index"
   devise_for :users
   resources :users, only: [:index, :show] do
