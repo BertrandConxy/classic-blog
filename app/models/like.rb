8584abddcf76efe308d5b97c5cd31e27f2f6dc81
bertrand <mutanganabertrand@gmail.com>
2022-10-17 12:19:01 +0200

    Add validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: true
+  validates :user_id, :post_id, presence: true
+
 
   def saved?
     like = Like.where(post: post, user: user)
